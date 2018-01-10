f00cfbd3fa17bf46c5025bf49a35707ee720b7c0
dhananjayaS <dhananjaya@wso2.com>
2018-01-10 08:08:42 +0530

    test123

--- com.vogella.build.maven.java/src/main/java/com/vogella/build/maven/java/App.java
+++ com.vogella.build.maven.java/src/main/java/com/vogella/build/maven/java/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println( "Hello 1234444" );
+	System.out.println( "Hello 1234445" );
     }
 }
